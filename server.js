e81669509e30f2011e43f8d09a7ba8acea4ea4a8
Jan Blonde <janblonde@MacBook-Pro-van-Jan.local>
2020-07-30 13:36:51 +0200

    ssl2

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const fs = require('fs');
+const https = require('https');
 
 const PORT = 3000;
 const api = require('./routes/api')
@@ -15,6 +17,14 @@ app.get('/',function(req,res){
   res.send('Hello from server')
 })
 
-app.listen(PORT,function(){
+https.createServer({
+  key: fs.readFileSync('/etc/ssl/private/www_sndx_be.key'),
+  cert: fs.readFileSync('/etc/ssl/certs/www_sndx_be.crt')
+}, app)
+.listen(PORT, function () {
   console.log('Server running')
 })
+
+// app.listen(PORT,function(){
+//   console.log('Server running')
+// })
